70a718f2957bd85015fd712963f8ac459903d8e0
Ananya1812 <ananyakini04@gmail.com>
2024-04-15 11:01:08 +0530

    created routes and tested all requests in bruno

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
+const port = 3000;
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const mongodburi = process.env.MONGODB_URI
 
-app.get('/', (req, res) => {
-  res.json({ message: 'pong' }); 
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+// Connecting to MongoDB
+mongoose.connect(mongodburi)
+
+// Defining a route to check MongoDB status
+app.get('/connection', (req, res) => {
+  if (mongoose.connection.readyState === 1) {
+    console.log("connected")
+    res.status(200).json({ message: 'Mongo DB is successfully connected' });
+  } else {
+    res.status(400).json({ message: 'Error connecting to Mongo DB' });
+  }
 });
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`🚀 server running on PORT: ${port}`);
+    console.log(`🚀 Server running on PORT: ${port}`);
   });
 }
 
-module.exports = app;
+module.exports = app;
